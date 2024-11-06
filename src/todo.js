bed841f9a9daaf3fca1d91da94be0e8b308e2d31
Finn <112250336+finn200102@users.noreply.github.com>
2024-11-06 15:24:02 +0100

    Add Background color and simple gridlayout

--- src/todo.js
+++ src/todo.js
@@ -99,9 +99,12 @@ const clearContent = () => {
 };
 
 const renderTasks = (function () {
-  const renderTaskpage = (tasks) => {
+  const renderTaskpage = (tasks, title) => {
     clearContent();
     const content = document.getElementById("content");
+    const contentTitle = document.createElement("h2");
+    contentTitle.textContent = title;
+    content.appendChild(contentTitle);
 
     const template = document.getElementById("todo-item-template");
 
@@ -109,10 +112,12 @@ const renderTasks = (function () {
     for (let i = 0; i < tasks.length; i++) {
       const element = template.content.cloneNode(true).children[0];
       const todoTitle = element.querySelector("#todo-item-title");
-      console.log(tasks[i]);
-
       todoTitle.textContent = tasks[i].title;
-      console.log(todoTitle.textContent);
+      const todoDueDate = element.querySelector("#todo-item-dueDate");
+      todoDueDate.textContent = format(
+        new Date(tasks[i].dueDate),
+        "dd/MM/yyyy"
+      );
       const todoDescription = element.querySelector("#todo-item-description");
       todoDescription.textContent = tasks[i].description;
       taskHolder.appendChild(element);
@@ -125,13 +130,16 @@ const renderTasks = (function () {
 
 export const tasksPage = (function () {
   const renderTaskPage = () => {
-    renderTasks.renderTaskpage(TodoManager.listTodos());
+    renderTasks.renderTaskpage(TodoManager.listTodos(), "Todos");
   };
   const renderDueTodayPage = () => {
-    renderTasks.renderTaskpage(TodoManager.dueToday());
+    renderTasks.renderTaskpage(TodoManager.dueToday(), "Due Today");
   };
   const renderUpcommingNextWeek = () => {
-    renderTasks.renderTaskpage(TodoManager.upcommingNextWeek());
+    renderTasks.renderTaskpage(
+      TodoManager.upcommingNextWeek(),
+      "Due next Week"
+    );
   };
   return { renderTaskPage, renderDueTodayPage, renderUpcommingNextWeek };
 })();
